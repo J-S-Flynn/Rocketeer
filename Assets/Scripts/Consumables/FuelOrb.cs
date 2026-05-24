f5c5ca0989a645df877be62e6fe2bf0c23bb867b
J-S-Flynn <j.s.flynn@outlook.com>
2026-05-24 20:58:55 +0100

    moved sphere coliders to orbs, added sound effect for collection, added left and right thrusters

--- Assets/Scripts/Consumables/FuelOrb.cs
+++ Assets/Scripts/Consumables/FuelOrb.cs
@@ -1,36 +1,37 @@
 using UnityEngine;
 
-namespace Consumables
+namespace Assets.Scripts.Consumables
 {
     public class FuelOrb : MonoBehaviour
     {
+        // Primatives
         private float _bounceHeight; 
         private float _bounceSpeed;
         private float _shrinkSpeed;
         private float _moveSpeed;
 
         private bool _isShrinking; 
-        
         private Vector3 _startPosition;
+        
+        // Componenets
+        private SphereCollider _sphereCollider; 
         private Transform _player;
-        private Collider _collider;
+
+        // Audio
+        private AudioSource _audioSauce;
+        private AudioClip _orbCollectionSfx;
+        
 
         private void Awake()
         {
-            _bounceHeight = 0.1f;
-            _bounceSpeed = 2.5f;
-            _shrinkSpeed = 1f;
-            _moveSpeed = 5f; 
-            
-            _isShrinking = false;
-            
-            _startPosition = transform.position;
-            _collider = GetComponent<Collider>();
-            
-            _collider.isTrigger = true; 
-            _player = GameObject.FindWithTag("Player").transform;
+            SetFuleOrb();
+
+            SetPlayer();
+
+            SetAudio();
         }
 
+        #region Game Loop 
         private void Update()
         {
             if (!_isShrinking)
@@ -42,19 +43,70 @@ namespace Consumables
         private void FixedUpdate()
         {
 
-
             if (_isShrinking)
             {
                Shrink();
             }
         }
         
+        #endregion
+
+        #region Setup 
+
+        public void SetFuleOrb()
+        {
+            _bounceHeight = 0.1f;
+            _bounceSpeed = 2.5f;
+            _shrinkSpeed = 1f;
+            _moveSpeed = 5f; 
+            
+            _isShrinking = false;
+            
+            _startPosition = transform.position;
+
+            _sphereCollider = GetComponent<SphereCollider>();
+
+            if (_sphereCollider == null){
+                
+                _sphereCollider = gameObject.AddComponent<SphereCollider>();
+            }
+            
+            _sphereCollider.isTrigger = true; 
+        }
+
+        public void SetPlayer()
+        {
+            _player = GameObject.FindWithTag("Player").transform;
+        }
+
+        #endregion
+
+        #region Audio & Visual
+
+        public void SetAudio()
+        {
+            GameObject audio = new GameObject("AudioSource");
+            
+            audio.transform.SetParent(transform); 
+
+            _audioSauce = audio.AddComponent<AudioSource>(); 
+
+            _audioSauce.volume = 5f; 
+
+            _orbCollectionSfx = Resources.Load<AudioClip>("Audio/Collectibles/OrbCollectionSfx");
+        }
+
+        #endregion
+
+        #region Behaviour
+
         private void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.CompareTag("Player"))
             {
                 _isShrinking = true;
-                _collider.enabled = false;
+                _sphereCollider.enabled = false;
+                _audioSauce.PlayOneShot(_orbCollectionSfx); 
             }
         }
 
@@ -73,5 +125,7 @@ namespace Consumables
                 Destroy(gameObject);
             }   
         }
+
+        #endregion
     }
 }
